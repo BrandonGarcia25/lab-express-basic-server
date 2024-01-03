6feeb346ca0d46313b89057907bcdb23e6e15b15
BrandonGarcia25 <bgbrandongarcia@gmail.com>
2024-01-03 12:01:41 -0400

    lab done

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+require('dotenv').config()
 
+var express = require ('express')
+var logger = require ('morgan')
+
+var apiRouter = require('./routes/api')
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+var app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(logger ('dev'))
+
+app.get('/', (req, res, next) => {
+
+    res.sendFile(__dirname + '/views/home.html')
+
+})
+
+app.get('/blog', (req, res, next) => {
+
+    res.sendFile(__dirname + '/views/blog.html')
+})
+
+app.use('/api', apiRouter)
 
 
 
@@ -23,3 +46,7 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(process.env.PORT, () => {
+    console.log("server is listening on " + process.env.PORT)
+})
